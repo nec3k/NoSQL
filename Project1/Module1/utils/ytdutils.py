2cd7a675e2213ad200b572c99abf4ec21a9f5527
nec3k <>
2023-11-19 20:34:14 +0100

    modified:   Project1/Module1/utils/ytdutils.py

--- Project1/Module1/utils/ytdutils.py
+++ Project1/Module1/utils/ytdutils.py
@@ -2,18 +2,21 @@ import os
 import yt_dlp
 
 
-
 class FilenameCollectorPP(yt_dlp.postprocessor.common.PostProcessor):
-    def __init__(self):
+    def __init__(self, outtmpl):
         super(FilenameCollectorPP, self).__init__(None)
         self.filenames = []
+        if os.path.split(outtmpl)[1].startswith("%(creator)s - "):
+            self.filename_starts_w_creator = True
+        else:
+            self.filename_starts_w_creator = False
 
     def run(self, information):
         filepath = information['filepath']
         filepath_splitted = os.path.split(filepath)
         filedir = filepath_splitted[0]
         filename = filepath_splitted[1]
-        if filename.startswith("NA - "):
+        if self.filename_starts_w_creator and filename.startswith("NA - "):
             filename = filename[5:]
             os.rename(filepath, os.path.join(filedir, filename))
         self.filenames.append(filename)
